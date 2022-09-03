f9a905331cec4202b5a1ca846dc8aa371270070c
Danilo <danilo.justino1@gmail.com>
2022-09-03 17:02:45 -0300

    SpS + Optimization

--- js.js
+++ js.js
@@ -1,23 +1,57 @@
 var score = 0;
 var clickUpgrade = 1;
-function myFunction(){
-    score += powerClick();
-    document.getElementById("score").innerHTML = "Score: " + score;
-    document.getElementById("erro").innerHTML = "";
+var SpSUpgrade = 0;
+
+//CLICK
+function generateScore(){
+    score += clickPower();
+    updateScore();
 }
 
+//SpS
+setInterval(SpS, 100);
+function SpS(){
+    score += SpSUpgrade / 10;
+    updateScore();
+}
+
+//UPGRADES
 function clickUpgradeBuy(){
     if(score >= clickUpgrade * 2){
         score -= clickUpgrade * 2;
         clickUpgrade ++;
-        document.getElementById("score").innerHTML = "Score: " + score;
-        document.getElementById("buyClickUpgrade").innerHTML = "Buy Upgrade: " + clickUpgrade * 2;
-        document.getElementById("clickPower").innerHTML = "Click Power: " + powerClick();
+        updateUI();
     }else{
-        document.getElementById("erro").innerHTML = "Não tem dinheiro";
+        error("Não tem dinheiro");
+    }
+}
+function SpSUpgradeBuy(){
+    if(score >= SpSUpgrade * 2){
+        score -= SpSUpgrade * 2;
+        SpSUpgrade ++;
+        updateUI();
+    }else{
+        error("Não tem dinheiro");
     }
 }
 
-function powerClick(){
+//ClickPower
+function clickPower(){
     return clickUpgrade;
+}
+
+//UI
+function updateUI(){
+    updateScore()
+    document.getElementById("buyClickUpgrade").innerHTML = "Buy Click Upgrade: " + clickUpgrade * 2;
+    document.getElementById("clickPower").innerHTML = "Click Power: " + clickPower();
+    document.getElementById("buySpSUpgrade").innerHTML = "Buy SpS Upgrade: " + SpSUpgrade * 2;
+    document.getElementById("SpS").innerHTML = "SpS: " + SpSUpgrade;
+    error("");
+}
+function updateScore(){
+    document.getElementById("score").innerHTML = "Score: " + score.toFixed(0);
+}
+function error(texto){
+    document.getElementById("erro").innerHTML = texto;
 }
